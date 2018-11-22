fa642c9b7dd839ed50c659e86d4e996bc6f03feb
olawolemoses <olawolemoses@gmail.com>
2018-11-22 09:00:43 +0100

    updated with tests 1

--- tests/DeveloperContactTest.php
+++ tests/DeveloperContactTest.php
@@ -8,8 +8,8 @@ class DeveloperContactTest extends TestCase
     /**
      * /products [GET]
      */
-    public function testShouldReturnAllProducts(){
-        $this->get("developers", []);
+    public function testShouldReturnAllDeveloperContacts(){
+        $this->get("api/developers", []);
         $this->seeStatusCode(200);
 
         $this->seeJsonStructure([
@@ -22,11 +22,12 @@ class DeveloperContactTest extends TestCase
                     'linkedin',
                     'phoneno',
                     'country',
-                    'developer_category'
+                    'developer_category',
+                    'links'
                 ]
             ],
             'meta' => [
-                '*' => [
+                'pagination' => [
                     'total',
                     'count',
                     'per_page',
@@ -38,81 +39,27 @@ class DeveloperContactTest extends TestCase
         ]);
 
     }
-    /**
-     * /products/id [GET]
-     */
-    public function testShouldReturnProduct(){
-        $this->get("products/2", []);
+
+    public function testShouldReturnDeveloperContact(){
+        $this->get("/api/developers/2", []);
         $this->seeStatusCode(200);
         $this->seeJsonStructure(
             ['data' =>
                 [
-                    'product_name',
-                    'product_description',
-                    'created_at',
-                    'updated_at',
-                    'links'
-                ]
-            ]
-        );
-
-    }
-    /**
-     * /products [POST]
-     */
-    public function testShouldCreateProduct(){
-        $parameters = [
-            'product_name' => 'Infinix',
-            'product_description' => 'NOTE 4 5.7-Inch IPS LCD (3GB, 32GB ROM) Android 7.0 ',
-        ];
-        $this->post("products", $parameters, []);
-        $this->seeStatusCode(200);
-        $this->seeJsonStructure(
-            ['data' =>
-                [
-                    'product_name',
-                    'product_description',
-                    'created_at',
-                    'updated_at',
-                    'links'
-                ]
-            ]
-        );
-
-    }
-
-    /**
-     * /products/id [PUT]
-     */
-    public function testShouldUpdateProduct(){
-        $parameters = [
-            'product_name' => 'Infinix Hot Note',
-            'product_description' => 'Champagne Gold, 13M AF + 8M FF 4G Smartphone',
-        ];
-        $this->put("products/4", $parameters, []);
-        $this->seeStatusCode(200);
-        $this->seeJsonStructure(
-            ['data' =>
-                [
-                    'product_name',
-                    'product_description',
-                    'created_at',
-                    'updated_at',
-                    'links'
+                  'firstname',
+                  'lastname',
+                  'email',
+                  'skypeid',
+                  'linkedin',
+                  'phoneno',
+                  'country',
+                  'developer_category',
+                  'links'
                 ]
             ]
         );
     }
-    /**
-     * /products/id [DELETE]
-     */
-    public function testShouldDeleteProduct(){
 
-        $this->delete("products/5", [], []);
-        $this->seeStatusCode(410);
-        $this->seeJsonStructure([
-                'status',
-                'message'
-        ]);
-    }
+  
+
 }
