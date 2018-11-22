04163b993512411b467645e022e0bf09e73914bb
olawolemoses <olawolemoses@gmail.com>
2018-11-22 09:16:48 +0100

    updated tests 2

--- tests/DeveloperContactTest.php
+++ tests/DeveloperContactTest.php
@@ -6,7 +6,7 @@ use Laravel\Lumen\Testing\DatabaseTransactions;
 class DeveloperContactTest extends TestCase
 {
     /**
-     * /products [GET]
+     * api/developers [GET]
      */
     public function testShouldReturnAllDeveloperContacts(){
         $this->get("api/developers", []);
@@ -40,6 +40,10 @@ class DeveloperContactTest extends TestCase
 
     }
 
+    /**
+     * api/developers/id [GET]
+     */
+
     public function testShouldReturnDeveloperContact(){
         $this->get("/api/developers/2", []);
         $this->seeStatusCode(200);
@@ -60,6 +64,75 @@ class DeveloperContactTest extends TestCase
         );
     }
 
-  
+    /**
+         * api/category/ [POST]
+         */
+        public function testShouldCreateDeveloperContact(){
+
+            $parameters = [
+                "firstname"=> "Deola1",
+                "lastname"=> "Habib1",
+                "email"=> "deola.habbib1@gmail.com",
+                "skypeid"=> "deola1.habbib1",
+                "linkedin"=> "http://www.linkedin.com/deola-habbib1",
+                "phoneno"=> "2347065396751",
+                "country"=> "Nigeria"
+            ];
+
+            $this->post("api/category/fullstack", $parameters, []);
+            $this->seeStatusCode(200);
+            $this->seeJsonStructure(
+                ['data' =>
+                      [
+                        'firstname',
+                        'lastname',
+                        'email',
+                        'skypeid',
+                        'linkedin',
+                        'phoneno',
+                        'country',
+                        'developer_category',
+                        'links'
+                      ]
+                ]
+            );
+
+        }
+
+        /**
+         * /products/id [PUT]
+         */
+        public function testShouldUpdateProduct(){
+            $parameters = [
+                'product_name' => 'Infinix Hot Note',
+                'product_description' => 'Champagne Gold, 13M AF + 8M FF 4G Smartphone',
+            ];
+            $this->put("products/4", $parameters, []);
+            $this->seeStatusCode(200);
+            $this->seeJsonStructure(
+                ['data' =>
+                    [
+                        'product_name',
+                        'product_description',
+                        'created_at',
+                        'updated_at',
+                        'links'
+                    ]
+                ]
+            );
+        }
+        /**
+         * /products/id [DELETE]
+         */
+        public function testShouldDeleteProduct(){
+
+            $this->delete("products/5", [], []);
+            $this->seeStatusCode(410);
+            $this->seeJsonStructure([
+                    'status',
+                    'message'
+            ]);
+        }
+
 
 }
